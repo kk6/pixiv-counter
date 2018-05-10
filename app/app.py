b8822e82cffdee9889e722f2f3c378a460716196
kk6 <hiro.ashiya@gmail.com>
2018-05-10 23:17:07 +0900

    Implement word view.

--- app/app.py
+++ app/app.py
@@ -1,11 +1,27 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
-from .models import SearchResult
+from flask import Flask, render_template, abort
+from .models import SearchResult, Word
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def index_view():
     results = SearchResult.select()
     return render_template('index.html', results=results)
+
+
+@app.route("/words/")
+def words_view():
+    words = Word.select()
+    return render_template('words.html', words=words)
+
+
+@app.route("/words/<string:word_text>")
+def word_view(word_text):
+    try:
+        word = Word.get(text=word_text)
+    except Word.DoesNotExist:
+        abort(404)
+    return render_template('word.html', results=word.results, word_text=word_text)
+
