c83d0377f5cec9eba65cf838c2d180550a5d4f38
kk6 <hiro.ashiya@gmail.com>
2018-05-14 16:47:28 +0900

    Added more information to display.

--- app/app.py
+++ app/app.py
@@ -7,11 +7,29 @@ from .models import SearchResult, Word
 app = Flask(__name__)
 
 
+def deco(results, target_day):
+    yesterday = target_day - datetime.timedelta(days=1)
+    for r in results:
+        try:
+            yr = SearchResult.get(word=r.word, stored_at=yesterday)
+            r.all_increase = r.num_of_all - yr.num_of_all
+            r.safe_increase = r.num_of_safe - yr.num_of_safe
+            r.r18_increase = r.num_of_r18 - yr.num_of_r18
+
+            _r18_rate_increase = r.calc_r18_rate() - yr.calc_r18_rate()
+            r.r18_rate_increase = "{:.2}".format(_r18_rate_increase)
+
+        except SearchResult.DoesNotExist:
+            pass
+        yield r
+
+
 @app.route("/")
 def index_view():
     today = datetime.date.today()
     results = SearchResult.select().where(SearchResult.stored_at == today)
-    return render_template('index.html', results=results)
+
+    return render_template('index.html', results=deco(results, today))
 
 
 @app.route("/word/")
@@ -40,4 +58,4 @@ def date_list_view():
 def date_view(date_text):
     date = datetime.datetime.strptime(date_text, "%Y-%m-%d").date()
     results = SearchResult.select().where(SearchResult.stored_at == date)
-    return render_template('date.html', results=results, date_text=date_text)
+    return render_template('date.html', results=deco(results, date), date_text=date_text)
